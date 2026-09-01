e0e795ff8a6260c036a56c43639de4ce7e2b5f3b
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-09-01 16:30:25 +0900

    feat: add flat-do-while-seq plugin

--- src/plugins/seq_exp/flat-do-while-seq.test.ts
+++ src/plugins/seq_exp/flat-do-while-seq.test.ts
@@ -7,12 +7,12 @@ pluginTester({
     { code: `console.log("foo");`, output: `console.log("foo");` },
     { code: `("foo","bar");`, output: `("foo", "bar");` },
     {
-      code: `do{console.log(1)}while((a = b,c = d))`,
+      code: `do{console.log(1)}while((a = b, true))`,
       output: `\
 do {
   console.log(1);
   a = b;
-} while ((c = d));`,
+} while (true);`,
     },
     {
       code: `do{const a = 1;console.log(a)}while((a = b,c = d))`,
@@ -20,6 +20,15 @@ do {
 do {
   const a = 1;
   console.log(a);
+} while (((a = b), (c = d)));`,
+    },
+    {
+      code: `do{const a = 1;console.log(a)}while((a = b,foo(),c = d))`,
+      output: `\
+do {
+  const a = 1;
+  console.log(a);
+  foo();
 } while (((a = b), (c = d)));`,
     },
   ],
--- src/plugins/seq_exp/flat-do-while-seq.ts
+++ src/plugins/seq_exp/flat-do-while-seq.ts
@@ -1,10 +1,76 @@
-import { type PluginObject, PluginPass } from "@babel/core";
+import { NodePath, type PluginObject, PluginPass } from "@babel/core";
 import * as t from "@babel/types";
+
+function getVariablesInBody(path: NodePath): Set<string> {
+  const vars = new Set<string>();
+
+  path.traverse({
+    VariableDeclarator(innerPath) {
+      if (t.isIdentifier(innerPath.node.id)) {
+        vars.add(innerPath.node.id.name);
+      }
+    },
+  });
+
+  return vars;
+}
+
+function canExtractExpression(
+  expression: t.Expression,
+  bodyVariables: Set<string>,
+): boolean {
+  if (!t.isAssignmentExpression(expression)) {
+    return true;
+  }
+
+  return !(
+    t.isIdentifier(expression.left) && bodyVariables.has(expression.left.name)
+  );
+}
+
 export default function (): PluginObject<PluginPass> {
   return {
     visitor: {
-      SequenceExpression(path) {
-        return;
+      DoWhileStatement(path) {
+        const { node } = path;
+        if (!t.isSequenceExpression(node.test)) {
+          return;
+        }
+
+        const expressions = node.test.expressions;
+        if (expressions.length < 2) {
+          return;
+        }
+
+        const bodyVariables = getVariablesInBody(path.get("body"));
+        const prefixExpressions = expressions.slice(0, -1);
+        const extractableExpressions = prefixExpressions.filter((expression) =>
+          canExtractExpression(expression, bodyVariables),
+        );
+
+        if (extractableExpressions.length === 0) {
+          return;
+        }
+
+        const remainingExpressions = prefixExpressions.filter(
+          (expression) => !canExtractExpression(expression, bodyVariables),
+        );
+        const lastExpression = expressions[expressions.length - 1];
+
+        node.test =
+          remainingExpressions.length > 0
+            ? t.sequenceExpression([...remainingExpressions, lastExpression])
+            : lastExpression;
+
+        const statements = extractableExpressions.map((expression) =>
+          t.expressionStatement(expression),
+        );
+
+        if (t.isBlockStatement(node.body)) {
+          node.body.body.push(...statements);
+        } else {
+          node.body = t.blockStatement([node.body, ...statements]);
+        }
       },
     },
   };
